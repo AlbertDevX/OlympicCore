b3404b41fae259217191e62a022a881b665cc9f1
AlbertDevX <140860958+AlbertDevX@users.noreply.github.com>
2024-12-27 16:32:07 -0400

    Update SalvarCommand.java
    soon command

--- src/main/java/com/albert/commands/SalvarCommand.java
+++ src/main/java/com/albert/commands/SalvarCommand.java
@@ -1,26 +1,6 @@
-package com.albert.commands;
+//---------------
+//𝕆𝕃𝕐𝕄ℙ𝕀ℂ ℂ𝕆ℝ𝔼
+//for version 1.0
+//---------------
 
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
-
-import net.md_5.bungee.api.ChatColor;
-
-public class SalvarCommand implements CommandExecutor {
-
-    public String prefix;
-    
-        @Override
-        public boolean onCommand(CommandSender sender, Command command, String alias, String[] args) {
-            if(!(sender instanceof Player)) {
-                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', " " + prefix + "&clos jugadores solo pueden usar este comando!"));
-                return true;
-            }
-
-            sender.sendMessage(ChatColor.translateAlternateColorCodes(
-                '&', " " + prefix + "&ceste comando estara disponible pronto!"));
-
-            return true;
-    }
-}
+//heal other soon
